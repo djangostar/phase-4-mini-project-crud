34289b281e229ca2b43e666e2da6c6c3106b06ec
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-09-19 14:17:36 -0700

    commit tree 4e568ec0056321416ef6d58530ad28f37c0a5f11

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,31 +1,48 @@
 class SpicesController < ApplicationController
-
-  def index
+rescue_from ActiveRecord::RecordNotFound, with: :render_errors
+  # GET /spices
+  def index 
     spices = Spice.all
     render json: spices
   end
 
-  def create
+  # GET /spices/:id
+  # def show
+  #   spice = find_spice
+  #   render json: spice 
+  # end 
+
+  # POST /spices
+  def create 
     spice = Spice.create(spice_params)
     render json: spice, status: :created
   end
-
+  
+  # PATCH /spices/:id
   def update
-    spice = Spice.find_by(id: params[:id])
+    spice = find_spice
     spice.update(spice_params)
     render json: spice
   end
 
-  def destroy
-    spice = Spice.find_by(id: params[:id])
+  # DELETE /spices/:id 
+  def destroy 
+    spice = find_spice
     spice.destroy
     head :no_content
   end
 
   private
-  
+
+  def find_spice
+    Spice.find(params[:id])
+  end
+
   def spice_params
     params.permit(:title, :image, :description, :notes, :rating)
   end
 
+  def render_errors
+    render json: { error: "Spice not found" }, status: :not_found
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_175524) do
+ActiveRecord::Schema.define(version: 2022_09_19_203649) do
 
   create_table "spices", force: :cascade do |t|
     t.string "title"
